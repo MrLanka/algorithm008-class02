df06031a69335bc1d7b6eec5bb08be99b654a731
MrLanka <46109111+MrLanka@users.noreply.github.com>
2020-04-29 22:10:17 +0800

    第17天每日刷题

---
+++ Week_01/d17_06_reversePrint.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if head==None:
+            return []
+        else:
+            return self.reversePrint(head.next)+[head.val]
